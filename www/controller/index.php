3221c56b7994aa75029245dba3f2b69686cfb154
hskim <nussland@gmail.com>
2015-05-04 09:49:07 +0900

    some update2

--- www/controller/index.php
+++ www/controller/index.php
@@ -5,6 +5,10 @@
  * Created on 15. 4. 30
  * @author: hskim
  */
+/*
+$conn = $db = new SQLite3('/var/db/bbs.db', SQLITE3_OPEN_READWRITE);
+$db->exec('CREATE TABLE user (id INTEGER PRIMARY KEY, username VARCHAR(50))');
+*/
 $f3 = require('lib/base.php');
 $f3->config('DEBUG', 2);
 $f3->config('AUTOLOAD', 'action/');
